0618fe8705cfec2fc84aec433dc58e82a781ea59
Ribas13 <diogo10ribeiro.dr@gmail.com>
2022-11-24 11:37:49 +0000

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:36:56 by diosanto          #+#    #+#             */
-/*   Updated: 2022/11/22 16:15:07 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/11/24 01:05:01 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,30 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}				t_list;
+
 //Prototypes
 
 char		*get_next_line(int fd);
+void		read_and_store(int fd, t_list **stash);
+void		extract_line(t_list *stash, char **line);
+int			found_new_line(t_list *stash);
+t_list		*ft_lst_get_last(t_list *stash);
+void		add_to_stash(t_list **stash, char *buff, int counter);
+void		generate_line(char **line, t_list *stash);
+void		clean_stash(t_list **stash);
+void		free_stash(t_list *stash);
+int			ftlen(const char *str);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,105 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:25:26 by diosanto          #+#    #+#             */
-/*   Updated: 2022/11/22 15:25:37 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/11/24 01:09:22 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+// 
+
+int	found_new_line(t_list *stash)
+{
+	int		i;
+	t_list	*current;
+
+	if (!stash)
+		return (0);
+	current = ft_lst_get_last(stash);
+	i = 0;
+	while (current->content[i])
+	{
+		if (current->content[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+t_list	*ft_lst_get_last(t_list *stash)
+{
+	t_list	*current;
+
+	current = stash;
+	while (current && current->next)
+		current = current->next;
+	return (current);
+}
+
+// Adds the content of the buffer to the end of the statsh
+
+void	add_to_stash(t_list **stash, char *buff, int counter)
+{
+	int		i;
+	t_list	*last;
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	new_node->next = NULL;
+	new_node->content = malloc(sizeof(char) * (counter + 1));
+	if (!new_node->content)
+		return ;
+	i = 0;
+	while (buff[i] && i < counter)
+	{
+		new_node->content[i] = buff[i];
+		i++;
+	}
+	if (!*stash)
+	{
+		*stash = new_node;
+		return ;
+	}
+	last = ft_lst_get_last(*stash);
+	last->next = new_node;
+}
+
+// Calculates the number of chars in the current line including the \n if there
+// is one and allocates memory
+
+void	generate_line(char **line, t_list *stash)
+{
+	int	i;
+	int	len;
+
+	len = 0;
+	while (stash)
+	{
+		i = 0;
+		while (stash->content[i])
+		{
+			if (stash->content[i] == '\n')
+			{
+				len++;
+				break ;
+			}
+			len++;
+			i++;
+		}
+		stash = stash->next;
+	}
+	*line = malloc(sizeof(char) * (len + 1));
+}
+
+int	ftlen(const char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*(str++))
+		len++;
+	return (len);
+}
